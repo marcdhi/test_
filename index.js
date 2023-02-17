88c7573a947b1d064ff98b5438c38136856f90d0
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-02-17 11:08:49 +0530

    Create index.js
    this file goes under the assets section

---
+++ index.js
@@ -0,0 +1,76 @@
+// This file just imports all assets and
+// exports them from one place
+
+import menu from "./menu.svg";
+import close from "./close.svg";
+import logo from "./incilogo.png";
+import logo2b from "./logo2b.png";
+import biz from "./coins/biz.png";
+import dance from "./coins/dance.png";
+import fashion from "./coins/fashion.png";
+import lit from "./coins/lit.png";
+import music from "./coins/music.png";
+import special from "./coins/special.png";
+import sports from "./coins/sports.png";
+
+import tandav from './cards/dance/tandav.jpg'
+import promenade from './cards/dance/promenade.jpg'
+import nritya from './cards/dance/nritya.jpg'
+import solo from './cards/dance/solo.jpg'
+import duet from './cards/dance/duet.jpg'
+
+import bisquiz from './cards/biz/BisQuiz.jpg'
+import hr from './cards/biz/HR.jpg'
+import manager from './cards/biz/Manager.jpg'
+import marketing from './cards/biz/Marketing.jpg'
+import operation from './cards/biz/Operations.jpg'
+
+import haute from './cards/fashion/haute.jpg'
+
+import pulse from './cards/music/pulse.jpg'
+import raag from './cards/music/RAAG.png'
+import bandish from './cards/music/BANDISH.jpg'
+
+import fine from './cards/specials/finearts.jpg'
+import gaming from './cards/specials/gaming.jpg'
+import hog from './cards/specials/hogathon.jpg'
+import stunt from './cards/specials/stunt.jpg'
+import world from './cards/specials/world.jpg'
+
+import spike from './cards/sports/spike.jpg'
+import slam from './cards/sports/slam.jpg'
+
+export {
+  menu,
+  close,
+  logo,
+  logo2b,
+  biz,
+  dance,
+  fashion,
+  lit,
+  music,
+  special,
+  sports,
+  tandav,
+  promenade,
+  nritya,
+  solo,
+  duet,
+  bisquiz,
+  hr,
+  manager,
+  marketing,
+  operation,
+  haute,
+  pulse,
+  raag,
+  bandish,
+  fine,
+  gaming,
+  hog,
+  stunt,
+  world,
+  spike,
+  slam
+};
